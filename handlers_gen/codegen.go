1d7b604f3edf19502ff8f1c7de3712b5ed95a263
axill86 <An970217>
2018-03-21 23:59:18 +0300

    added validation

--- handlers_gen/codegen.go
+++ handlers_gen/codegen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -11,7 +12,6 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-
 )
 
 type validationInfo struct {
@@ -23,14 +23,25 @@ type validationInfo struct {
 	replaceParamName bool
 	paramname        string
 	enumValues       []string
-	useDefault bool
+	useDefault       bool
 	defaultValue     string
 }
 
-type apigenComment struct {
-	URL    string `json:"url"`
-	Auth   bool   `json:"auth"`
-	Method string `json:"method"`
+type handlerInfo struct {
+	apigenInfo    apigenInfo
+	hanlderMethod string
+}
+type apigenInfo struct {
+	URL     string `json:"url"`
+	Auth    bool   `json:"auth"`
+	Method  string `json:"method"`
+	Handler string
+}
+
+// Route - route
+type route struct {
+	URL        string
+	MethodName string
 }
 
 func main() {
@@ -48,31 +59,152 @@ func main() {
 	fmt.Fprintln(out, `import "net/http"`)
 	fmt.Fprintln(out, `import "strconv"`)
 	fmt.Fprintln(out, `import "fmt"`)
-
+	fmt.Fprintln(out, `import "encoding/json"`)
+	fmt.Fprint(out, `
+type Body struct {
+	Error    string      "json:\"error\""
+	Response interface{} "json:\"response,omitempty\""
+}
+`)
 	fmt.Fprintln(out) // empty line
 
+	handlers := make(map[string][]handlerInfo)
 	for _, f := range node.Decls {
-		g, ok := f.(*ast.GenDecl)
-		if !ok {
-			fmt.Printf("SKIP %T is not *ast.GenDecl\n", f)
-			continue
-		}
-		for _, spec := range g.Specs {
-			currType, ok := spec.(*ast.TypeSpec)
-			if !ok {
-				fmt.Printf("SKIP %T is not ast.TypeSpec\n", spec)
-				continue
-			}
+		switch f.(type) {
+		case *ast.GenDecl:
+			g, _ := f.(*ast.GenDecl)
+			for _, spec := range g.Specs {
+				currType, ok := spec.(*ast.TypeSpec)
+				if !ok {
+					fmt.Printf("SKIP %T is not ast.TypeSpec\n", spec)
+					continue
+				}
 
-			currStruct, ok := currType.Type.(*ast.StructType)
-			if !ok {
-				fmt.Printf("SKIP %T is not ast.StructType\n", currStruct)
-				continue
-			}
+				currStruct, ok := currType.Type.(*ast.StructType)
+				if !ok {
+					fmt.Printf("SKIP %T is not ast.StructType\n", currStruct)
+					continue
+				}
 
-			CreateFillFunction(out, currType.Name.Name, currStruct.Fields.List)
-		}
+				CreateBindFunction(out, currType.Name.Name, currStruct.Fields.List)
+			}
+		case *ast.FuncDecl:
+			fun, _ := f.(*ast.FuncDecl)
+			if strings.HasPrefix(fun.Doc.Text(), "apigen:api") {
+				jsonStr := strings.TrimPrefix(fun.Doc.Text(), "apigen:api")
+
+				var apiInfo apigenInfo
+
+				err := json.Unmarshal([]byte(jsonStr), &apiInfo)
+				if err != nil {
+					log.Fatalf("Can't parse apigen instructions %s", jsonStr)
+				}
+
+				fmt.Fprintf(out, "//Generated Handler for function %s\n", fun.Name.Name)
+				receiverName := fun.Recv.List[0].Type.(*ast.StarExpr).X.(*ast.Ident).Name
+				paramsName := fun.Type.Params.List[len(fun.Type.Params.List)-1].Type.(*ast.Ident).Name
+				handlerName := fmt.Sprintf("%sHandler", fun.Name)
+				handlers[receiverName] = append(handlers[receiverName], handlerInfo{apigenInfo: apiInfo, hanlderMethod: handlerName})
+				fmt.Fprintf(out, `
+func (srv *%s) %s(w http.ResponseWriter, r *http.Request) {
+
+`, receiverName, handlerName)
+				if apiInfo.Auth {
+					fmt.Fprint(out, `
+if r.Header.Get("X-Auth") != "100500" {
+		w.WriteHeader(403)
+		body := Body{
+				Error: "unauthorized",
+			}
+		bodyBytes, _:= json.Marshal(body)
+		w.Write(bodyBytes)
+		return
 	}
+`)
+				}
+				if apiInfo.Method != "" {
+					fmt.Fprintf(out, `
+if r.Method != "%s" {
+		w.WriteHeader(406)
+		body := Body {
+			Error: "bad method",
+		}
+		bodyBytes, _:= json.Marshal(body)
+		w.Write(bodyBytes)
+		return
+	}
+`, apiInfo.Method)
+				}
+
+				fmt.Fprintf(out, `
+
+	params := %s{}
+	err := params.Bind(r)
+	w.Header()["Content-Type"] = []string{"application/json"}
+	if err != nil {
+		w.WriteHeader(400)
+		body := Body {
+			Error: err.Error(),
+		}
+		bodyBytes, _:= json.Marshal(body)
+		w.Write(bodyBytes)
+		return
+	}
+`, paramsName)
+
+				fmt.Fprintf(out, `
+result, err := srv.%s(r.Context(), params)
+	if err!=nil {
+		if apierror, ok := err.(ApiError); ok {
+			w.WriteHeader(apierror.HTTPStatus)
+		} else {
+			w.WriteHeader(500)
+		}
+		body := Body{
+				Error: err.Error(),
+			}
+		bodyBytes, _:= json.Marshal(body)
+		w.Write(bodyBytes)
+		return
+	}
+	body := Body{
+		Response: result,
+	}
+	bodyBytes, _ := json.Marshal(body)
+	w.Write(bodyBytes)
+`, fun.Name)
+				fmt.Fprint(out, "}\n")
+			}
+		}
+
+	}
+	//generating ServeHttp
+	for receiver, hs := range handlers {
+		fmt.Fprintf(out, `
+func (h *%s ) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+`, receiver)
+		for _, h := range hs {
+			fmt.Fprintf(out, `
+case "%s":
+		h.%s(w, r)
+`, h.apigenInfo.URL, h.hanlderMethod)
+		}
+
+		fmt.Fprint(out, `
+default:
+		w.WriteHeader(404)
+		body := Body{
+				Error: "unknown method",
+			}
+		bodyBytes, _:= json.Marshal(body)
+		w.Write(bodyBytes)
+		return
+	}
+}
+`)
+	}
+
 }
 
 func getValidationInfo(tag string) validationInfo {
@@ -107,7 +239,7 @@ func getValidationInfo(tag string) validationInfo {
 	return result
 }
 
-func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
+func CreateBindFunction(out io.Writer, structName string, fields []*ast.Field) {
 	fmt.Fprintf(out, "// Generating Bind Function for struct %s\n", structName)
 	fmt.Fprintf(out, "func (model *%s) Bind(r *http.Request) error {\n", structName)
 
@@ -119,7 +251,7 @@ func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
 			validatorTag, ok := tag.Lookup("apivalidator")
 			if ok {
 				vInfo := getValidationInfo(validatorTag)
-				paramName := field.Names[0].Name
+				paramName := strings.ToLower(field.Names[0].Name)
 				if vInfo.replaceParamName {
 					paramName = vInfo.paramname
 				}
@@ -127,7 +259,7 @@ func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
 				switch field.Type.(*ast.Ident).Name {
 				case "string":
 					fmt.Fprintf(out, "model.%s = r.FormValue(\"%s\") \n", field.Names[0].Name, paramName)
-					ValidateStringField(out, field, vInfo)
+					ValidateStringField(out,field.Names[0].Name, paramName, vInfo)
 					if vInfo.useDefault {
 						fmt.Fprintf(out, `if model.%s == "" {
 			model.%s = "%s"
@@ -135,8 +267,15 @@ func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
 `, field.Names[0].Name, field.Names[0].Name, vInfo.defaultValue)
 					}
 				case "int":
-					fmt.Fprintf(out, "model.%s, _ = strconv.Atoi(r.FormValue(\"%s\")) \n", field.Names[0].Name, paramName)
-					ValidateIntField(out, field, vInfo)
+					fmt.Fprintf(out, `
+val, err := strconv.Atoi(r.FormValue("%s"))
+if err != nil {
+	return fmt.Errorf("%s must be int")
+}
+model.%s = val
+ `,paramName, paramName, field.Names[0].Name)
+
+					ValidateIntField(out, field.Names[0].Name, paramName, vInfo)
 				}
 
 			}
@@ -146,36 +285,36 @@ func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
 	}
 	fmt.Fprintln(out, "return nil")
 	fmt.Fprint(out, "}\n")
+
 }
 
-func ValidateStringField(out io.Writer, field *ast.Field, vInfo validationInfo) {
-	println ("validation for field ", field.Names[0].Name)
+func ValidateStringField(out io.Writer, field string, paramName string, vInfo validationInfo) {
 	if vInfo.required {
 		fmt.Fprintf(out, `
 if model.%s == "" {
-		return fmt.Errorf("Field %s is required")
+		return fmt.Errorf("%s must me not empty")
 	}
-`, field.Names[0].Name, field.Names[0].Name)
+`, field, paramName)
 	}
 
 	if vInfo.hasMax {
 		fmt.Fprintf(out, `
 if len(model.%s) > %d {
-		return fmt.Errorf("Field %s should not be longer than %d")
+		return fmt.Errorf("%s len must be >= %d")
 	}
-`, field.Names[0].Name, vInfo.max, field.Names[0].Name, vInfo.max)
+`, field, vInfo.max, paramName, vInfo.max)
 	}
 
 	if vInfo.hasMin {
 		fmt.Fprintf(out, `
 if len(model.%s) < %d {
-		return fmt.Errorf("Field %s should not be shorter than %d")
+		return fmt.Errorf("%s len must be >= %d")
 	}
-`, field.Names[0].Name, vInfo.min, field.Names[0].Name, vInfo.min)
+`, field, vInfo.min, paramName, vInfo.min)
 	}
 
 	if len(vInfo.enumValues) > 0 {
-		fmt.Fprintf(out, "switch model.%s {\n", field.Names[0].Name)
+		fmt.Fprintf(out, "switch model.%s {\n", field)
 		for _, enumValue := range vInfo.enumValues {
 			fmt.Fprintf(out, "case \"%s\":\n", enumValue)
 		}
@@ -183,34 +322,35 @@ if len(model.%s) < %d {
 			fmt.Fprint(out, "case \"\":\n")
 		}
 		fmt.Fprintf(out, `default:
-		return fmt.Errorf("Field %s should contain one of the following %v")
+		return fmt.Errorf("%s must be one of [%s]")
 	}
-`, field.Names[0].Name, vInfo.enumValues)
+`, paramName, strings.Join(vInfo.enumValues, ", "))
 	}
 
 }
 
-func ValidateIntField(out io.Writer, field *ast.Field, vInfo validationInfo) {
+func ValidateIntField(out io.Writer, field string, paramName string, vInfo validationInfo) {
 	if vInfo.required {
 		fmt.Fprintf(out, `
 if model.%s == 0 {
 		return fmt.Errorf("Field %s is required")
 	}
-`, field.Names[0].Name, field.Names[0].Name)
+`, field, paramName)
 	}
 	if vInfo.hasMax {
 		fmt.Fprintf(out, `
 if model.%s > %d {
-		return fmt.Errorf("Field %s should not be  larger than %d")
+		return fmt.Errorf("%s must be <= %d")
 	}
-`, field.Names[0].Name, vInfo.max, field.Names[0].Name, vInfo.max)
+`, field, vInfo.max, paramName, vInfo.max)
 	}
 
 	if vInfo.hasMin {
 		fmt.Fprintf(out, `
 if model.%s < %d {
-		return fmt.Errorf("Field %s should be smaller than %d")
+		return fmt.Errorf("%s must be >= %d")
 	}
-`, field.Names[0].Name, vInfo.min, field.Names[0].Name, vInfo.min)
+`, field, vInfo.min, paramName, vInfo.min)
 	}
+
 }
