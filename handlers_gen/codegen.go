61048b6cc3f73ffb5a1e716ad72f30c829a966d4
axill86 <An970217>
2018-03-21 18:22:50 +0300

    added validation

--- handlers_gen/codegen.go
+++ handlers_gen/codegen.go
@@ -5,35 +5,33 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
-	"text/template"
+
 )
 
-type tpl struct {
-	FieldName string
+type validationInfo struct {
+	required         bool
+	hasMin           bool
+	hasMax           bool
+	min              int
+	max              int
+	replaceParamName bool
+	paramname        string
+	enumValues       []string
+	useDefault bool
+	defaultValue     string
 }
 
-var (
-	intTpl = template.Must(template.New("intTpl").Parse(`
-	// {{.FieldName}}
-	var {{.FieldName}}Raw uint32
-	binary.Read(r, binary.LittleEndian, &{{.FieldName}}Raw)
-	in.{{.FieldName}} = int({{.FieldName}}Raw)
-`))
-
-	strTpl = template.Must(template.New("strTpl").Parse(`
-	// {{.FieldName}}
-	var {{.FieldName}}LenRaw uint32
-	binary.Read(r, binary.LittleEndian, &{{.FieldName}}LenRaw)
-	{{.FieldName}}Raw := make([]byte, {{.FieldName}}LenRaw)
-	binary.Read(r, binary.LittleEndian, &{{.FieldName}}Raw)
-	in.{{.FieldName}} = string({{.FieldName}}Raw)
-`))
-)
+type apigenComment struct {
+	URL    string `json:"url"`
+	Auth   bool   `json:"auth"`
+	Method string `json:"method"`
+}
 
 func main() {
 	fset := token.NewFileSet()
@@ -44,10 +42,13 @@ func main() {
 
 	out, _ := os.Create(os.Args[2])
 
+	fmt.Fprintln(out, `// Generated code `)
 	fmt.Fprintln(out, `package `+node.Name.Name)
 	fmt.Fprintln(out) // empty line
-	fmt.Fprintln(out, `import "encoding/binary"`)
-	fmt.Fprintln(out, `import "bytes"`)
+	fmt.Fprintln(out, `import "net/http"`)
+	fmt.Fprintln(out, `import "strconv"`)
+	fmt.Fprintln(out, `import "fmt"`)
+
 	fmt.Fprintln(out) // empty line
 
 	for _, f := range node.Decls {
@@ -69,34 +70,7 @@ func main() {
 				continue
 			}
 
-			putValidateFunction := false
-
-			for _, field := range currStruct.Fields.List {
-
-				if field.Tag != nil {
-
-					println("tags", field.Tag.Value)
-					tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1])
-					validatorTag, ok := tag.Lookup("apivalidator")
-					if ok {
-						if !putValidateFunction {
-							putValidateFunction = true
-							fmt.Fprintf(out, "func ( input * %s) validate () bool { \n", currType.Name)
-						}
-						vInfo := getValidationInfo(validatorTag)
-						if (vInfo.required) {
-							fmt.Fprintf(out, "if input.%s == \"\" {return false} \n", field.Names[0].Name)
-						}
-
-					}
-
-				}
-
-			}
-			if putValidateFunction {
-				fmt.Fprint(out, "return true \n}\n")
-			}
-
+			CreateFillFunction(out, currType.Name.Name, currStruct.Fields.List)
 		}
 	}
 }
@@ -104,17 +78,22 @@ func main() {
 func getValidationInfo(tag string) validationInfo {
 	result := validationInfo{}
 	for _, param := range strings.Split(tag, ",") {
-		pairs := strings.Split(param, "")
+		if strings.HasPrefix(param, "required") {
+			result.required = true
+			continue
+		}
+		pairs := strings.Split(param, "=")
 		key := pairs[0]
 		value := pairs[1]
+
 		switch key {
-		case "required":
-			result.required = true
 		case "paramname":
+			result.replaceParamName = true
 			result.paramname = value
 		case "enum":
 			result.enumValues = strings.Split(value, "|")
 		case "default":
+			result.useDefault = true
 			result.defaultValue = value
 		case "min":
 			result.hasMin = true
@@ -124,16 +103,114 @@ func getValidationInfo(tag string) validationInfo {
 			result.max, _ = strconv.Atoi(value)
 		}
 	}
+	fmt.Println(result)
 	return result
 }
 
-type validationInfo struct {
-	required     bool
-	hasMin       bool
-	hasMax       bool
-	min          int
-	max          int
-	paramname    string
-	enumValues   []string
-	defaultValue string
+func CreateFillFunction(out io.Writer, structName string, fields []*ast.Field) {
+	fmt.Fprintf(out, "// Generating Bind Function for struct %s\n", structName)
+	fmt.Fprintf(out, "func (model *%s) Bind(r *http.Request) error {\n", structName)
+
+	for _, field := range fields {
+
+		if field.Tag != nil {
+
+			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1])
+			validatorTag, ok := tag.Lookup("apivalidator")
+			if ok {
+				vInfo := getValidationInfo(validatorTag)
+				paramName := field.Names[0].Name
+				if vInfo.replaceParamName {
+					paramName = vInfo.paramname
+				}
+
+				switch field.Type.(*ast.Ident).Name {
+				case "string":
+					fmt.Fprintf(out, "model.%s = r.FormValue(\"%s\") \n", field.Names[0].Name, paramName)
+					ValidateStringField(out, field, vInfo)
+					if vInfo.useDefault {
+						fmt.Fprintf(out, `if model.%s == "" {
+			model.%s = "%s"
+		}
+`, field.Names[0].Name, field.Names[0].Name, vInfo.defaultValue)
+					}
+				case "int":
+					fmt.Fprintf(out, "model.%s, _ = strconv.Atoi(r.FormValue(\"%s\")) \n", field.Names[0].Name, paramName)
+					ValidateIntField(out, field, vInfo)
+				}
+
+			}
+
+		}
+
+	}
+	fmt.Fprintln(out, "return nil")
+	fmt.Fprint(out, "}\n")
+}
+
+func ValidateStringField(out io.Writer, field *ast.Field, vInfo validationInfo) {
+	println ("validation for field ", field.Names[0].Name)
+	if vInfo.required {
+		fmt.Fprintf(out, `
+if model.%s == "" {
+		return fmt.Errorf("Field %s is required")
+	}
+`, field.Names[0].Name, field.Names[0].Name)
+	}
+
+	if vInfo.hasMax {
+		fmt.Fprintf(out, `
+if len(model.%s) > %d {
+		return fmt.Errorf("Field %s should not be longer than %d")
+	}
+`, field.Names[0].Name, vInfo.max, field.Names[0].Name, vInfo.max)
+	}
+
+	if vInfo.hasMin {
+		fmt.Fprintf(out, `
+if len(model.%s) < %d {
+		return fmt.Errorf("Field %s should not be shorter than %d")
+	}
+`, field.Names[0].Name, vInfo.min, field.Names[0].Name, vInfo.min)
+	}
+
+	if len(vInfo.enumValues) > 0 {
+		fmt.Fprintf(out, "switch model.%s {\n", field.Names[0].Name)
+		for _, enumValue := range vInfo.enumValues {
+			fmt.Fprintf(out, "case \"%s\":\n", enumValue)
+		}
+		if vInfo.useDefault {
+			fmt.Fprint(out, "case \"\":\n")
+		}
+		fmt.Fprintf(out, `default:
+		return fmt.Errorf("Field %s should contain one of the following %v")
+	}
+`, field.Names[0].Name, vInfo.enumValues)
+	}
+
+}
+
+func ValidateIntField(out io.Writer, field *ast.Field, vInfo validationInfo) {
+	if vInfo.required {
+		fmt.Fprintf(out, `
+if model.%s == 0 {
+		return fmt.Errorf("Field %s is required")
+	}
+`, field.Names[0].Name, field.Names[0].Name)
+	}
+	if vInfo.hasMax {
+		fmt.Fprintf(out, `
+if model.%s > %d {
+		return fmt.Errorf("Field %s should not be  larger than %d")
+	}
+`, field.Names[0].Name, vInfo.max, field.Names[0].Name, vInfo.max)
+	}
+
+	if vInfo.hasMin {
+		fmt.Fprintf(out, `
+if model.%s < %d {
+		return fmt.Errorf("Field %s should be smaller than %d")
+	}
+`, field.Names[0].Name, vInfo.min, field.Names[0].Name, vInfo.min)
+	}
 }
